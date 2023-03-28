fc37c32df7181d75f02f73e07606ccc5cfba69b2
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-03-28 17:57:11 +0530

    added style to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
+import './categories.styles.scss'
+
 const App = () => {
+
+  const categories = [
+    {
+      id : 1,
+      title: 'Hats',
+      
+    },
+    {
+      id : 2,
+      title: 'Jackets',
+      
+    },
+    {
+      id : 3,
+      title: 'Sneakers',
+      
+    },
+    {
+      id : 4,
+      title: 'Womens',
+      
+    },
+    {
+      id : 5,
+      title: 'Mens',
+      
+    }
+  ]
+
   return (
-    <div>
-    Hello world s
-    </div>
-  )
+    <div className="categories-container">
+    {categories.map(({title, id}) => (
+      <div key={id} className="category-container">
+      <div className="background-image" />
+      <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>ShopNow</p>
+      </div>
+      </div>   
+      ))}
+     </div>
+     )
 }
 
 export default App
