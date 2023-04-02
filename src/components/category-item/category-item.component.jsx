ca0ba30fbd9e3803d21bcaba4259fe37d89db924
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-04-02 22:27:30 +0530

    added catogory comp

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import './category-item.styles.scss';
+
+const categoryItem = ({category}) => {
+    const {imageUrl,title} = category
+    return(
+          <div className="category-container">
+          <div 
+          className="background-image" 
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }} />
+          <div className="category-body-container">
+          <h2>{title}</h2>
+          <p>ShopNow</p>
+          </div>
+          </div>   
+       
+    )
+}
