4dd33314e3dfc15c50a38a69ef7dc731fe2a6539
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-04-07 09:48:59 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from "./routes/home/home.component"
 const App = () => {
  return(
    <Routes>
-    <Route path='/home' element={ <Home/> }/>
+    <Route path='/' element={ <Home/> }/>
    </Routes>
   ); 
      
