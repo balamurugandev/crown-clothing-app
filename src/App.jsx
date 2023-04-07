a507a47bc5cf1740cd6d69013e0dea4003267677
Balamurugan <108758172+balamurugandev@users.noreply.github.com>
2023-04-07 12:20:36 +0000

    added navigation page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Routes,Route } from 'react-router-dom'
-
+import { Routes,Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 import Home from "./routes/home/home.component"
+
 
 const App = () => {
  return(
    <Routes>
-    <Route path='/home' element={ <Home/> }/>
+    <Route path='/' element={<Navigation />}>
+      <Route index element={<Home />} />
+    </Route>
    </Routes>
-  ); 
-     
+  );    
 }
 
 export default App
