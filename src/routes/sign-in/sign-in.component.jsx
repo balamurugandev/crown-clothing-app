7f8537cda23d3cb73d63ed64451bd6b3e1063d3e
Balamurugan S <balamurugans@Balamurugans-Mac-mini.local>
2023-04-15 20:17:47 +0530

    added firebase storage

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,11 @@
-import { signInWithGooglePopup  } from "../../utils/firebase/firebase.utils";
+import { signInWithGooglePopup , createUserDocumentfromAuth } from "../../utils/firebase/firebase.utils";
+
 
 const SignIn = () => {
 
    const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    const {user} = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentfromAuth(user);
 
 
    }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,13 @@ import {
       signInWithPopup,
       GoogleAuthProvider
        } from 'firebase/auth'
+
+import {
+       getFirestore,
+       doc,
+       getDoc,
+       setDoc
+} from 'firebase/firestore'
  
 const firebaseConfig = {
     apiKey: "AIzaSyDn-ywSPbGOxCLXsZcLJLW6xCV7mn1GShw",
@@ -27,6 +34,34 @@ const firebaseConfig = {
   export const auth = getAuth();
   export const signInWithGooglePopup = () =>
    signInWithPopup(auth, provider);
+
+   export const db = getFirestore();
+
+   export const createUserDocumentfromAuth = async (userAuth) => {
+     const userDocRef = doc(db, 'users', userAuth.uid);
+
+
+     const userSnapshot = await getDoc(userDocRef);
+
+   if(!userSnapshot.exists()){
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+   
+
+   try {
+    await setDoc(userDocRef , {
+        displayName,
+        email,
+        createdAt
+    });
+    } catch(error) {
+        console.log('error creating the user',error.message);
+    }
+   } 
+
+   return userDocRef;
+
+   }
 
 
   
